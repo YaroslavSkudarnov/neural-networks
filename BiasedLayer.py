9f724a3badaf111949dc78b37a9baa1b96163489
Iaroslav Skudarnov <Iaroslav.Skudarnov@kronshtadt.ru>
2017-03-27 21:45:17 +0300

    implement BiasedLayer

--- BiasedLayer.py
+++ BiasedLayer.py
@@ -1,4 +1,8 @@
 from SimpleLayer import SimpleLayer
 
 class BiasedLayer(SimpleLayer):
-    pass
+    def __init__(self, input_neurons, output_neurons=1):
+        super().__init__(input_neurons + 1, output_neurons)
+
+    def output(self, input_vector):
+        return super().output(input_vector + [1.0])

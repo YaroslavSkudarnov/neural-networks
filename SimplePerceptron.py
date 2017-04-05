d179bbc8f3cc9ed640abecb597e54c1761df6581
Iaroslav Skudarnov <Iaroslav.Skudarnov@kronshtadt.ru>
2017-04-05 18:01:48 +0300

    refactoring

--- SimplePerceptron.py
+++ SimplePerceptron.py
@@ -2,11 +2,8 @@ from BiasedLayer import BiasedLayer
 from Neuron import Neuron
 
 class SimplePerceptron:
-    def __init__(self, input_neurons, start_multiplier=1, fade_multiplier=0.95, steps=100):
+    def __init__(self, input_neurons):
         self.__input_layer = BiasedLayer(input_neurons)
-        self.__start_multiplier = start_multiplier
-        self.__fade_multiplier = fade_multiplier
-        self.__steps = steps
 
     def __train_step_single_vector(self, input_vector, output, multiplier):
         delta = output - self.output(input_vector)
@@ -14,13 +11,16 @@ class SimplePerceptron:
 
     def __train_step(self, input_vectors, outputs):
         multiplier = self.__start_multiplier
-        for input_vector, output in input_vectors, outputs:
+        for input_vector, output in zip(input_vectors, outputs):
             self.__train_step_single_vector(input_vector, output, multiplier)
             multiplier *= self.__fade_multiplier
         
-    def train(self, input_vectors, outputs):
-        for _ in range(self.__steps):
-            self.__train_step(input_vecs, outputs)
+    def train(self, input_vectors, outputs, start_multiplier=0.05, fade_multiplier=1, steps=100):
+        self.__start_multiplier = start_multiplier
+        self.__fade_multiplier = fade_multiplier
+
+        for i in range(steps):
+            self.__train_step(input_vectors, outputs)
 
     def output(self, input_vector):
         return self.__input_layer.output(input_vector)

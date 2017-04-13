84ecc0b266c4127f21c3bf40b6c0c2081e1e2536
Iaroslav Skudarnov <Iaroslav.Skudarnov@kronshtadt.ru>
2017-04-13 21:52:36 +0300

    add multiplier reusing

--- SimplePerceptron.py
+++ SimplePerceptron.py
@@ -8,19 +8,23 @@ class SimplePerceptron:
 
     def __train_step_single_vector(self, input_vector, output, multiplier):
         delta = output - self.output(input_vector)
-        self.__input_layer = BiasedLayer([neuron.update(self.__multiplier * delta * inp) for neuron, inp in zip(self.__input_layer, input_vector + [1.0])])
         logging.info(input_vector)
+        logging.info('Output before: {}'.format(self.output(input_vector)))
+        self.__input_layer = BiasedLayer([neuron.update(self.__multiplier * delta * inp) for neuron, inp in zip(self.__input_layer, input_vector + [1.0])])
         logging.info(', '.join(str(x) for x in self.__input_layer._SimpleLayer__neurons))
+        logging.info('Output after: {}'.format(self.output(input_vector)))
 
     def __train_step(self, input_vectors, outputs):
         inputs_and_outputs = list(zip(input_vectors, outputs))
         random.shuffle(inputs_and_outputs)
+        logging.info(inputs_and_outputs)
 
         for input_vector, output in inputs_and_outputs:
             self.__train_step_single_vector(input_vector, output, self.__multiplier)
         
-    def train(self, input_vectors, outputs, start_multiplier=0.5, fade_multiplier=0.95, steps=100):
-        self.__multiplier = start_multiplier #TODO: REUSE MULTIPLIER
+    def train(self, input_vectors, outputs, start_multiplier=0.5, fade_multiplier=0.95, steps=100, reuse_multiplier=False):
+        if not reuse_multiplier:
+            self.__multiplier = start_multiplier
         for i in range(steps):
             self.__train_step(input_vectors, outputs)
             self.__multiplier *= fade_multiplier
